f42fb07b470a237b58c0152b8cd8aa13a6cc1473
xxsyferr <106472745+xxsyferr@users.noreply.github.com>
2022-05-29 19:38:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
+const Discord = require("discord.js");
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
+const prefix = "!";
+
+const fs = require("fs");
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
+}
+
+client.once("ready", () => {
+  console.log("i am up");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "payment") {
+    client.commands.get("payment").execute(message, args, Discord);
+  }
+});
+
+client.login(
+  "OTc4Nzg3NzgwMDkwMDIzOTg3.Gk9uv3.S-ylqcQ3W9y0OJXVig6DnF609KfthaILzLeTfI"
+);
